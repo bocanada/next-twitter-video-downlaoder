4515f0c96e29a4664a441bcd56d8d8d6b48f6ff4
Bocanada <24578415+Bocanada@users.noreply.github.com>
2022-04-15 01:15:08 -0300

    fixed env var

--- utils/getVideos.ts
+++ utils/getVideos.ts
@@ -3,7 +3,7 @@ import { Data } from '../pages/api/video/[id]';
 export const getVideos = async (id: string) => {
 	try {
 		// Make a call to [base url]/api/video/[ID]
-		const resp = await fetch(`https://${process.env.VERCEL_URL}/api/video/${id}`);
+		const resp = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/video/${id}`);
 		const json: Data = await resp.json();
 		return json;
 	} catch (e) {
